777b0e127b734ceb3979461c29a4380d4c6f7928
Aditya kumar rai <130901374+Adityaraikasyap@users.noreply.github.com>
2025-09-28 01:31:40 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -30,7 +30,7 @@ import dmiac from './assets/tech_logo/dmaic.jpg'
 import six from './assets/tech_logo/sixsigma.png'
 
 // Experience Section Logo's
-import webverseLogo from './assets/company_logo/webverse_logo.png';
+import webverseLogo from './assets/company_logo/iit.png';
 // import agcLogo from './assets/company_logo/agc_logo.png';
 // import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
 
